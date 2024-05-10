7caa1996e7b101162924e666432d761b9a90481a
zaidanpasya <147239142+zprxzai@users.noreply.github.com>
2024-05-10 15:00:55 +0700

    Add files via upload

--- tampil.php
+++ tampil.php
@@ -389,8 +389,8 @@ family=Rubik+Gemstones&family=Titan+One&family=Trirong:ital,wght@0,100;0,200;0,3
           <div class="sub-dropdown">
             <button class="dropbtn">Konten</button>
             <div class="sub-dropdown-content">
-              <a href="#">Budaya</a><br>
-              <a href="#">Bangunan</a>
+            <a href="?u=budaya"><span style="color: black;">Budaya</span></a><br>
+              <a href="?u=Bangunan"><span style="color: black;">Bangunan</span></a>
             </div>
           </div>
         </div>
